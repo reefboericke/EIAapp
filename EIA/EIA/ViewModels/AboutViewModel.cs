2e12f0ed38eea91868daeecfbcda67e6188d43a1
= <reef.boericke@gmail.com>
2022-11-03 15:29:51 +0000

    fix routing for add button

--- EIA/EIA/ViewModels/AboutViewModel.cs
+++ EIA/EIA/ViewModels/AboutViewModel.cs
@@ -8,14 +8,18 @@ namespace EIA.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        public ICommand OpenWebCommand { get; }
+        public ICommand GoToItems { get; }
         public AboutViewModel()
         {
             Title = "Compare";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
-            GoToItems = new Command(async () => await Shell.Current.GoToAsync(nameof(ItemsPage)));
+            GoToItems = new Command(OnGoToSearch);
         }
 
-        public ICommand OpenWebCommand { get; }
-        public ICommand GoToItems { get; }
+        async void OnGoToSearch()
+        {
+            await Shell.Current.GoToAsync("//ItemsPage");
+        }
     }
 }
